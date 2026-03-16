dac504f329a189ca46d2fcebd110c51d2818ebea
YunusGafur <127ygafur@frhsd.com>
2026-03-16 12:51:16 -0400

    first commit

---
+++ java.js
@@ -0,0 +1,25 @@
+let favFilm = ["Avatar","Matrix","Toy Story"];
+
+const hobbies = new Array("Climbing","Hanging out","Video Games");
+const luckyNumbers = new Array(1,2,3);
+
+const cars = ["toyota","Nissan","Ford"];
+let myNewCar = cars[2];
+
+const fortune = ["yes","no","maybe","ask again"];
+
+console.log(fortune[2]);
+fortune[0] = "Absolutely!";
+fortune[1] = "NO! WRONG!";
+fortune[2] = "Diddy 67 Perhaps";
+fortune[3] = "I couldn't hear you";
+console.log(fortune[0]);
+console.log(fortune[1]);
+console.log(fortune[2]);
+console.log(fortune[3]);
+
+const superlong = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15];
+let length = superlong.length;
+console.log(superlong.length)
+
+superlong[superlong.length-1] = 1000000;
